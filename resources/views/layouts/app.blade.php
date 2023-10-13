5f2721b8955606376c97328d61fb9a7cb9d40e6b
umbertofaenza <umberto.faenza@gmail.com>
2023-10-13 16:31:12 +0200

    use env('APP_NAME') in navbar and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,16 +2,16 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+    <title>{{ env('APP_NAME') }} - {{ $page_title }}</title>
 
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-  <!-- Styles -->
-  @vite('resources/js/app.js')
+    <!-- Styles -->
+    @vite('resources/js/app.js')
 </head>
 
 <body>
@@ -24,7 +24,7 @@
             @yield('main-content')
         </div>
     </main>
-  
+
 </body>
 
 </html>
