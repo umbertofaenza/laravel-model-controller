90f38a33715c07aa92fa8d5b838ae843f69bfa4a
umbertofaenza <umberto.faenza@gmail.com>
2023-10-13 13:27:40 +0200

    connect pagecontroller and homepage

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    function index()
+    {
+        return view('homepage');
+    }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,7 +15,7 @@
 </head>
 
 <body>
-  <h1>ciao!</h1>
+  <h1>Home</h1>
 </body>
 
 </html>
